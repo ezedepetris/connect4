d1e95c7e26bf077f2a006f3fbd011d873a05dec7
ezedepetris <ezedepetris@gmail.com>
2015-11-09 20:40:41 -0300

    [#107773902] method value for grid allready done

--- src/main/java/com/unrc/app/Grid.java
+++ src/main/java/com/unrc/app/Grid.java
@@ -9,6 +9,7 @@ public class Grid extends Model{
 	private int row;
 	private int column;
 	private int tokens;
+	private int level;
 
 	/*CONSTRUCTOR*/
 	/*this method build the structure of an board, and sets the atributs of this method*/
@@ -19,11 +20,6 @@ public class Grid extends Model{
 		tokens = 0;
 	}
 
-	public int turn(){
-		if(tokens%2 == 0)
-			return 1;
-		return 2;
-	}
 
 	public Grid(int arow, int aColumn){
 		grid = new Cell[arow][aColumn];
@@ -36,85 +32,152 @@ public class Grid extends Model{
 				grid[i][j]=null;
 	}
 
+	public boolean isMin(){
+		return level == -1;
+	}
+
+	public boolean isMax(){
+		return level == 1;
+	}
+
+	public int turn(){
+		if(tokens%2 == 0)
+			return 1;
+		return 2;
+	}
+
 /*this method search a winner of this game*/
-	public int searchWinner(Integer player){int count = 0;
+	public int scountSequence(Integer player){
+		int score = 0;
+		int count = 0;
 		/*this verify the column if some player wins*/
 		for(int i=0; i<=row; i++){
 			count = 0;
 			for (int j=0 ;j<column; j++){
-				if(grid[i][j]==null||grid[i][j+1] ==null)
+				if(grid[i][j]==null||grid[i][j+1] ==null){
+					if(count == 2)
+						score += 100
+					if(count == 3)
+						score +=1000
+					if(count == 4)
+						score +=10000
 					count =0;
+				}
 				else{
 					if(player.compareTo(grid[i][j].getCell()) == 0 && 0 ==player.compareTo(grid[i][j+1].getCell())){
 						count++;
-						if (count >= 3)
-							return player;
 					}
-					else
+					else{
+						if(count == 2)
+							score += 100
+						if(count == 3)
+							score +=1000
+						if(count == 4)
+							score +=10000
 						count = 0;
+					}
 				}
 			}	
 		}
 		/*this verify the row if some player wins*/
+		int count = 0;
 		for(int i=0; i<=column; i++){
 			count = 0;
 			for (int j=0 ;j<row; j++){
-				if(grid[j][i]==null||grid[j+1][i] ==null)
+				if(grid[j][i]==null||grid[j+1][i] ==null){
+					if(count == 2)
+						score += 100
+					if(count == 3)
+						score +=1000
+					if(count == 4)
+						score +=10000
 					count =0;
+				}
 				else{
 					if(player.compareTo(grid[j][i].getCell())  == 0 && 0 == player.compareTo(grid[j+1][i].getCell())){
 						count++;
-						if (count >= 3)
-							return player;
 					}
-					else
+					else{
+						if(count == 2)
+							score += 100
+						if(count == 3)
+							score +=1000
+						if(count == 4)
+							score +=10000
 						count = 0;
+					}
 				}
 			}	
 		}
 		/*this verify the diagonal left toright if some player wins*/
+		int count = 0;
 		for(int i=0; i<column; i++){
 			count = 0;
 			int ii = i;
 			for (int j=0 ;j<row; j++){
 				if(ii<column){
-					if(grid[j][ii]==null||grid[j+1][ii+1] ==null)
+					if(grid[j][ii]==null||grid[j+1][ii+1] ==null){
+						if(count == 2)
+							score += 100
+						if(count == 3)
+							score +=1000
+						if(count == 4)
+							score +=10000
 						count =0;
+					}
 					else{
 						if(player.compareTo(grid[j][ii].getCell())  == 0 && 0 == player.compareTo(grid[j+1][ii+1].getCell())){
 							count++;
 							ii++;
-							if (count >= 3)
-								return player;
 						}
-						else
+						else{
+							if(count == 2)
+								score += 100
+							if(count == 3)
+								score +=1000
+							if(count == 4)
+								score +=10000
 							count = 0;
+						}
 					}
 				}
 			}	
 		}
 		/*this verify the diagonal right to left if some player wins*/
+		int count = 0;
 		for(int i=column; i>0; i--){
 			count = 0;
 			int ii = i;
 			for (int j=0 ;j<row; j++){
 				if(ii>0){
-					if(grid[j][ii] == null || grid[j+1][ii-1] == null)
+					if(grid[j][ii] == null || grid[j+1][ii-1] == null){
+						if(count == 2)
+							score += 100
+						if(count == 3)
+							score +=1000
+						if(count == 4)
+							score +=10000
 						count =0;
+					}
 					else{
 						if(player.compareTo(grid[j][ii].getCell())  == 0 && 0 == player.compareTo(grid[j+1][ii-1].getCell())){
 							count++;
 							ii--;
-							if (count >= 3)
-								return player;
 						}
-						else
+						else{
+							if(count == 2)
+								score += 100
+							if(count == 3)
+								score +=1000
+							if(count == 4)
+								score +=10000
 							count = 0;
+						}
 					}
 				}	
 			}
 		}
-		return 0 ;
+		return score ;
 	}
 // 		return 0; //if no body wins, this returns 0, that is equal to said, for doubt no body wins
 // 		//return 1 ;//= ganadaron el jugador n 1
@@ -219,6 +282,14 @@ public class Grid extends Model{
 		return half;
 	}
 
+	public int value(Grid state){
+		int pc = this.countSequence(2);
+		int human = this.countSequence(1);
+		if(this.isMax(state))
+			return pc-human;
+		else
+			return human-pc;
+	}
 
 	/*load a old game*/
 	public int load(List<Cell> listCells){
@@ -291,7 +362,85 @@ public String next(){
 
 
 
-
+	public int searchWinner(Integer player){int count = 0;
+		/*this verify the column if some player wins*/
+		for(int i=0; i<=row; i++){
+			count = 0;
+			for (int j=0 ;j<column; j++){
+				if(grid[i][j]==null||grid[i][j+1] ==null)
+					count =0;
+				else{
+					if(player.compareTo(grid[i][j].getCell()) == 0 && 0 ==player.compareTo(grid[i][j+1].getCell())){
+						count++;
+						if (count >= 3)
+							return player;
+					}
+					else
+						count = 0;
+				}
+			}	
+		}
+		/*this verify the row if some player wins*/
+		for(int i=0; i<=column; i++){
+			count = 0;
+			for (int j=0 ;j<row; j++){
+				if(grid[j][i]==null||grid[j+1][i] ==null)
+					count =0;
+				else{
+					if(player.compareTo(grid[j][i].getCell())  == 0 && 0 == player.compareTo(grid[j+1][i].getCell())){
+						count++;
+						if (count >= 3)
+							return player;
+					}
+					else
+						count = 0;
+				}
+			}	
+		}
+		/*this verify the diagonal left toright if some player wins*/
+		for(int i=0; i<column; i++){
+			count = 0;
+			int ii = i;
+			for (int j=0 ;j<row; j++){
+				if(ii<column){
+					if(grid[j][ii]==null||grid[j+1][ii+1] ==null)
+						count =0;
+					else{
+						if(player.compareTo(grid[j][ii].getCell())  == 0 && 0 == player.compareTo(grid[j+1][ii+1].getCell())){
+							count++;
+							ii++;
+							if (count >= 3)
+								return player;
+						}
+						else
+							count = 0;
+					}
+				}
+			}	
+		}
+		/*this verify the diagonal right to left if some player wins*/
+		for(int i=column; i>0; i--){
+			count = 0;
+			int ii = i;
+			for (int j=0 ;j<row; j++){
+				if(ii>0){
+					if(grid[j][ii] == null || grid[j+1][ii-1] == null)
+						count =0;
+					else{
+						if(player.compareTo(grid[j][ii].getCell())  == 0 && 0 == player.compareTo(grid[j+1][ii-1].getCell())){
+							count++;
+							ii--;
+							if (count >= 3)
+								return player;
+						}
+						else
+							count = 0;
+					}
+				}	
+			}
+		}
+		return 0 ;
+	}
 
 
 
